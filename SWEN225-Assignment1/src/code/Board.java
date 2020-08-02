f5588fcadf3821a0843bdfb458886b38af05e324
Feldor-Lex <38927234+Feldor-Lex@users.noreply.github.com>
2020-08-03 00:42:29 +1200

    Map reading/loading
    May require further changes (IE, rendering doors), but a rudimentary map file can be loaded at the very least.

--- SWEN225-Assignment1/src/code/Board.java
+++ SWEN225-Assignment1/src/code/Board.java
@@ -1,5 +1,11 @@
 package code;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.util.Scanner;
+
 public class Board {
 
 	// ------------------------
@@ -12,6 +18,10 @@ public class Board {
 	// Board Associations
 	private Game game;
 
+	//Constants
+	static final int width=24;
+	static final int height=25;
+
 	// ------------------------
 	// CONSTRUCTOR
 	// ------------------------
@@ -19,14 +29,78 @@ public class Board {
 	public Board(Game aGame) {
 		characters = new Character[6];
 		weapons = new Weapon[6];
-		cells = new Location[24][25];//One-letter initiation guide since the map's going to be hard-coded:
+		cells = loadMap();
+	}
+
+	private Location[][] loadMap(){
+		Location[][] toReturn = new Location[width][height];//One-letter initiation guide since the map's going to be hard-coded:
+
+
+		try {
+			Scanner sc = new Scanner(new File("map.txt"));
+			System.out.println("Map loaded:");
+			for (int i = 0; i < height; i++) {
+				String line = sc.nextLine();
+
+				for (int j = 0; j < width; j++) {
+					toReturn[j][i] = loadLocationFromChar(line.charAt(j));
+					//System.out.print(line.charAt(j));//Debug purposes only.
+				}
+				//System.out.println("");//Debug purposes only
+
+			}
+
+			return toReturn;
+		}catch(IOException e){System.out.println("ERROR LOADING MAP:"+e);}
+		System.out.println("CRITICAL ERROR LOADING MAP");//If it didn't return in the try/catch function then something has gone horribly wrong.
+		return null;
+	}
+
+	public Location loadLocationFromChar(char c){
 		//1-9 are room tiles, they may or may not be able to be walked into
 		//1:Ball room 2:Conservatory 3:Billiard room 4:Library 5:Study
-		// 6:Hall 7:Lounge 8:Dining Room 9:Kitchen
+		//6:Hall 7:Lounge 8:Dining Room 9:Kitchen
 		//n: Null space, can't be walked into
 		//h: hallway, free access to walk into
 		//r: Room space. Not a doorway, but should be rendered as "empty space" rather than a wall.
+		switch (c){
+			case 'n':
+				return null;
+			case 'h':
+				return new Hallway();
+			case 'r':
+				return new Room("");
+			case '1':
+				return (new Room("Ball room"));
+			case '2':
+				return (new Room("Conservatory"));
+			case '3':
+				return (new Room("Billiard room"));
+			case '4':
+				return (new Room("Library"));
+			case '5':
+				return (new Room("Study"));
+			case '6':
+				return (new Room("Hall"));
+			case '7':
+				return (new Room("Lounge"));
+			case '8':
+				return (new Room("Dining room"));
+			case '9':
+				return (new Room("Kitchen"));
+			default: System.out.println("ERROR! UNEXPECTED TYPE! TRIED TO GENERATE A LOCATION USING INVALID CHAR:"+c);
+			return null;
+		}
 
+
+	}
+
+	public Location getCell(int xVal, int yVal){
+		return cells[xVal][yVal];
+	}
+
+	public void setCell(int xVal, int yVal, Location l){
+		cells[xVal][yVal]=l;
 	}
 
 	// ------------------------
